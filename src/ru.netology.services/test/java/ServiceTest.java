6cfb2b33731bab1c3ad1180980b87b2a654968f5
Sillis Kan <it.kan@bk.ru>
2023-06-30 21:38:00 +0500

    load work

--- src/ru.netology.services/test/java/ServiceTest.java
+++ src/ru.netology.services/test/java/ServiceTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class ServiceTest {
 
     @Test
@@ -11,20 +9,4 @@ public class ServiceTest {
         int result = new Service(10000, 3000, 20000).calculate();
         Assertions.assertEquals(expected, result);
     }
-
-//    @Test
-//    void shouldCalculateForRegisteredAndOverLimit() {
-//        BonusService service = new BonusService();
-//
-//        // подготавливаем данные:
-//        long amount = 1_000_000;
-//        boolean registered = true;
-//        long expected = 500;
-//
-//        // вызываем целевой метод:
-//        long actual = service.calculate(amount, registered);
-//
-//        // производим проверку (сравниваем ожидаемый и фактический):
-//        Assertions.assertEquals(expected, actual);
-//    }
 }
